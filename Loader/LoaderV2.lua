52f3679d8c44ce42b8f550713744e257b3b95b84
NoLag2 <csy@tracecollege.edu.ph>
2025-07-15 18:42:57 +0800

    Update LoaderV2.lua

--- Loader/LoaderV2.lua
+++ Loader/LoaderV2.lua
@@ -1,2 +1,12 @@
+old_identify = identifyexecutor
+fake_identify = function()
+    real_exec = old_identify()
+    if real_exec == "Delta" then
+        return "Delta_Allowed"
+    end
+    return real_exec
+end
+identifyexecutor = fake_identify
+getgenv().identifyexecutor = fake_identify
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/isDpMIR6LU/0", true))()
