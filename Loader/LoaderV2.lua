e2b7349a16e2518e678851095bec045af492d6f7
NoLag2 <csy@tracecollege.edu.ph>
2025-07-16 09:49:07 +0800

    Update LoaderV2.lua

--- Loader/LoaderV2.lua
+++ Loader/LoaderV2.lua
@@ -1,2 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
 loadstring(game:HttpGet("https://cdn.sourceb.in/bins/MCKw77S0vs/0", true))()
+loadstring(game:HttpGet("https://pastefy.app/8YQyyNV7/raw"))()
