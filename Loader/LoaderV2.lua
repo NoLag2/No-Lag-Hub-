d9ce35b63b0c1f15f8b188a5d34cb8237821040d
NoLag2 <csy@tracecollege.edu.ph>
2025-08-19 16:29:16 +0800

    Update LoaderV2.lua

--- Loader/LoaderV2.lua
+++ Loader/LoaderV2.lua
@@ -1,3 +1,3 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Loader/Main.lua"))()
-loadstring(game:HttpGet("https://api.rubis.app/v2/scrap/yjWnKIqGY5t8FETn/raw", true))()
-loadstring(game:HttpGet("https://pastefy.app/UyqKb4jU/raw"))()
+ID="edaa9d20-68d5-4d77-adb3-ccbc02e6e534";
+loadstring(game:HttpGet("https://cdn.reverse.software/loader.luau"))()
